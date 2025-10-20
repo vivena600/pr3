34685dc018049f1c5b932b8b87f900a8ac6b36f7
Даша Головина <golovinad84@gmail.com>
2025-10-20 13:49:59 +0500

    added 6 pr

--- api.py
+++ api.py
@@ -2,9 +2,4 @@ from fastapi import FastAPI
 from todo import todo_router
 
 app = FastAPI()
-
-@app.get("/")
-async def welcome() -> dict:
-    return {"message": "Добро пожаловать в мое приложение!"}
-
 app.include_router(todo_router)
--- model.py
+++ model.py
@@ -1,17 +1,43 @@
-from pydantic import BaseModel
+from typing import List, Optional
+from fastapi import Form
+from pydantic import BaseModel, ConfigDict
 
-class Todo(BaseModel):
-    id: int
+class TodoItem(BaseModel):
     item: str
 
-class Item(BaseModel):
-    item: str
-    status: str
-
-    class Config:
-        schema_extra = {
+    model_config = ConfigDict(
+        json_schema_extra={
             "example": {
-                "id": 1,
-                "item": "Пример задачи"
+                "item": "Read the next chapter of the book"
             }
         }
+    )
+
+class Todo(BaseModel):
+    id: Optional[int] = None
+    item: str
+
+    @classmethod
+    def as_form(
+        cls,
+        item: str = Form(...)
+    ):
+        return cls(item=item)
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
+    )
